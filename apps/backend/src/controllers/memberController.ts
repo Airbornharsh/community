886d864962ba5ae43e1a87e26721df1b9efb401a
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-04-16 02:12:28 +0530

    Added Add Member

--- apps/backend/src/controllers/memberController.ts
+++ apps/backend/src/controllers/memberController.ts
@@ -1,14 +1,128 @@
 import { RequestHandler } from 'express'
 import { NoUserResponse, catchErrorResponse } from '../constants/response'
 import { UserToken } from '../middlewares/authMiddleware'
+import db from '@repo/db/clients'
+import { v4 } from 'uuid'
 
 export const createMemberController: RequestHandler = async (req, res) => {
   try {
-    const user = res.locals.user as UserToken | null
-    if (user === null) {
+    const tempUser = res.locals.user as UserToken | null
+    if (tempUser === null) {
       return res.status(401).json(NoUserResponse)
     }
+    const { community, user, role } = req.body
+    const communityExists = await db.community.findFirst({
+      where: {
+        id: community
+      }
+    })
+    if (!communityExists) {
+      return res.status(400).json({
+        status: false,
+        errors: [
+          {
+            param: 'community',
+            message: 'Community not found.',
+            code: 'RESOURCE_NOT_FOUND'
+          }
+        ]
+      })
+    }
+    const memberWithUserRole = await db.member.findMany({
+      where: {
+        community: community
+      },
+      include: {
+        userRef: true,
+        roleRef: true
+      }
+    })
+
+    const admin = memberWithUserRole.find((member) => {
+      return (
+        member.userRef.id === tempUser.id &&
+        member.roleRef.name === 'Community Admin'
+      )
+    })
+    if (!admin) {
+      return res.status(401).json({
+        status: false,
+        errors: [
+          {
+            message: 'You are not authorized to perform this action.',
+            code: 'NOT_ALLOWED_ACCESS'
+          }
+        ]
+      })
+    }
+    const userExists = await db.user.findFirst({
+      where: {
+        id: user
+      }
+    })
+    if (!userExists) {
+      return res.status(400).json({
+        status: false,
+        errors: [
+          {
+            param: 'user',
+            message: 'User not found.',
+            code: 'RESOURCE_NOT_FOUND'
+          }
+        ]
+      })
+    }
+    const roleExists = await db.role.findFirst({
+      where: {
+        id: role
+      }
+    })
+    if (!roleExists) {
+      return res.status(400).json({
+        status: false,
+        errors: [
+          {
+            param: 'role',
+            message: 'Role not found.',
+            code: 'RESOURCE_NOT_FOUND'
+          }
+        ]
+      })
+    }
+    const memberExists = await db.member.findFirst({
+      where: {
+        community: community,
+        user: user
+      }
+    })
+    if (memberExists) {
+      return res.status(400).json({
+        status: false,
+        errors: [
+          {
+            message: 'User is already added in the community.',
+            code: 'RESOURCE_EXISTS'
+          }
+        ]
+      })
+    }
+    const member = await db.member.create({
+      data: {
+        id: v4(),
+        roleRef: { connect: { id: role } },
+        userRef: { connect: { id: user } },
+        communityRef: { connect: { id: community } },
+        created_at: new Date()
+      }
+    })
+    return res.status(200).json({
+      status: true,
+      content: {
+        data: member
+      }
+    })
   } catch (error) {
+    console.log(error)
     return res.status(500).json(catchErrorResponse)
   }
 }
