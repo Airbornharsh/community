66f6fa4f7faa242939d27998184867399cfe5ce4
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-04-15 16:06:05 +0530

    User Exists

--- apps/backend/src/controllers/userController.ts
+++ apps/backend/src/controllers/userController.ts
@@ -5,19 +5,39 @@ import db from '@repo/db/clients'
 
 export const signupController: RequestHandler = async (req, res) => {
   const { name, email, password } = req.body
-  const { error } = userValidation.validate({ name, email, password })
-  if (error) {
-    return res.status(400).json(error)
-  }
-
-  const user = await db.user.create({
-    data: {
-      id: v4(),
-      name,
-      email,
-      password,
-      created_at: new Date()
+  try {
+    const { error } = userValidation.validate({ name, email, password })
+    if (error) {
+      return res.status(400).json(error)
     }
-  })
-  return res.status(201).json(user)
+    console.log('1')
+    const userExists = await db.user.findFirst({
+      where: {
+        email
+      }
+    })
+    console.log(userExists)
+    if (userExists) {
+      return res.status(400).json({
+        status: false,
+        errors: [
+          {
+            param: 'email',
+            message: 'User with this email address already exists.',
+            code: 'RESOURCE_EXISTS'
+          }
+        ]
+      })
+    }
+    const user = await db.user.create({
+      data: {
+        id: v4(),
+        name,
+        email,
+        password,
+        created_at: new Date()
+      }
+    })
+    return res.status(201).json(user)
+  } catch (e) {}
 }
